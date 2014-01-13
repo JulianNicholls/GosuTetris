7f312d3b722ae86d8b5094e8286bac21caca1782
Julian Nicholls <juliannicholls29@gmail.com>
2014-01-13 19:51:19 +0000

    Block.draw now uses draw_absolute

--- block.rb
+++ block.rb
@@ -3,20 +3,19 @@
 require './constants'
 
 module Tetris
-
   # Draw a constituent block of a Tetris shape.
 
   class Block
     include Constants
 
-    def self.draw( window, row, column, colour )
-      window.draw_rectangle( WELL_BORDER + column * BLOCK_SIDE, row * BLOCK_SIDE,
-                             BLOCK_SIDE, BLOCK_SIDE, 1, Gosu::Color::WHITE )
+    # Draw in the well, indexed by cell
 
-      window.draw_rectangle( WELL_BORDER + column * BLOCK_SIDE + 1,
-                             row * BLOCK_SIDE + 1,
-                             BLOCK_SIDE - 2, BLOCK_SIDE - 2, 1, colour )
+    def self.draw( window, row, column, colour )
+      draw_absolute( window, WELL_BORDER + column * BLOCK_SIDE,
+                     row * BLOCK_SIDE, colour )
     end
+
+    # Draw at an absolute pixel position
 
     def self.draw_absolute( window, left, top, colour )
       window.draw_rectangle( left, top, BLOCK_SIDE, BLOCK_SIDE, 1,
